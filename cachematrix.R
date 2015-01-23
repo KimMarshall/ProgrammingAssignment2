42702268ae56388883ec01171af87b08b4100f2e
kim marshall <onthebay8@tampabay.rr.com>
2015-01-23 14:09:09 -0500

    a3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions caches processed matrixes and stores them.  Then when 
+## an identical matrix is given to these functions the answers are retrieved 
+## from cache rather than being recalculated. 
 
-## Write a short comment describing this function
+## makeCacheMatrix() a "matrix" object is created that caches the 
+##  input matrix and the inverse of the input matrix for use in cacheSolve
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = mx()) {
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        
+        get<-function() x
+        setmatrix<-function(solve) m<<- solve
+        getmatrix<-function() m
+        list(set=set, get=get,
+             setmatrix=setmatrix,
+             getmatrix=getmatrix)
 }
 
+## cachesolve() determines if the matrix has been previously cached 
+## and if so returns the cached solution if not calculates the inverse
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=mx(), ...) {
+        m<-x$getmatrix()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        mx<-x$get()
+        m<-solve(mx, ...)
+        x$setmatrix(m)
+        m
 }
